b705655cea45759a04029a93ad6cf2b5f0f95e95
Steveng1997 <stivengarcia1855@gmail.com>
2022-11-18 11:10:55 -0500

    cambios 96

--- src/app/pages/pre-auth/admin/reto-personal/reto-personal.component.ts
+++ src/app/pages/pre-auth/admin/reto-personal/reto-personal.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import Swal from 'sweetalert2';
-import { EstudianteService } from 'src/app/core/services/estudiantes.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoginService } from 'src/app/core/services/login.service';
+import { RetoService } from 'src/app/core/services/retos.service';
 
 @Component({
   selector: 'app-reto-personal',
@@ -10,13 +9,13 @@ import { LoginService } from 'src/app/core/services/login.service';
   styleUrls: ['./reto-personal.component.scss'],
 })
 export class RetoPersonalComponent implements OnInit {
+  retos: any[] = [];
   idUser: string;
-  estudiantes: any[] = [];
   public page!: number;
 
   constructor(
     public router: Router,
-    public serviceEstudiante: EstudianteService,
+    public serviceRetos: RetoService,
     public serviceLogin: LoginService,
     private activeRoute: ActivatedRoute
   ) {}
@@ -24,49 +23,18 @@ export class RetoPersonalComponent implements OnInit {
   ngOnInit(): void {
     this.idUser = this.activeRoute.snapshot.paramMap.get('id');
     this.serviceLogin.getById(this.idUser);
-    this.getEmpleados();
+    this.getRetos();
   }
 
   Agregar() {
     this.router.navigate([
-      `admin/${this.idUser}/insertar-estudiantes/${this.idUser}`,
+      `admin/${this.idUser}/insertar-retos/${this.idUser}`,
     ]);
   }
 
-  getEmpleados() {
-    this.serviceEstudiante.getEstudiantesAll().subscribe((datosEstudiantes) => {
-      this.estudiantes = datosEstudiantes;
-    });
-  }
-
-  DeleteUser(id) {
-    this.serviceEstudiante.getById(id).then((datoEstudiante) => {
-      if (datoEstudiante) {
-        Swal.fire({
-          title: '¿Deseas eliminar el registro?',
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Si, Deseo eliminar!',
-        }).then((result) => {
-          if (result.isConfirmed) {
-            Swal.fire({
-              position: 'top-end',
-              icon: 'success',
-              title: '¡Eliminado Correctamente!',
-              showConfirmButton: false,
-              timer: 2500,
-            });
-
-            this.serviceEstudiante.deleteEstudiante(
-              datoEstudiante[0]['idDocument'],
-              id
-            );
-            this.getEmpleados();
-          }
-        });
-      }
+  getRetos() {
+    this.serviceRetos.getRetoPersonalAll().subscribe((datosRetos) => {
+      this.retos = datosRetos;
     });
   }
 }
